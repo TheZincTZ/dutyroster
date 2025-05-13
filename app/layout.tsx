1148dc9372ce8f8840ec3724cb71aac81a3ba6fb
TheZInc <154614743+TheZincTZ@users.noreply.github.com>
2025-05-13 13:43:48 +0800

    icon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.jpg" type="image/jpeg" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
